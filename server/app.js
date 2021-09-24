cb47f4e59b3b4325a24b46a2c93449c303f07c7d
Darr Arellano <darrarellano@developersinstitute.co.nz>
2021-09-24 14:10:42 +1200

    add product listing

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const { application } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
+const products = require("/products");
 
 app.get("/products", (request, response) => {
-  response.send({ message: "Hello from server!" });
+  response.send(products);
 });
 
 app.listen(port, () => {
@@ -12,3 +13,29 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// const PropertyModel = require("./models/PropertyModel");
+// const formatProperty = require("./formaProperty");
+
+// app.use(cors());
+// app.use(express.json());
+
+// // FIXME write your routes here
+// app.get("/properties", async (request, response) => {
+//   const properties = await PropertyModel.find({});
+//   const formattedProperties = properties.map((property) => {
+//     return formatProperty(property);
+//   });
+//   return response.status(200).send(formattedProperties);
+// });
+// app.get("/properties/:id", async (request, response) => {
+//   const id = request.params.id;
+//   const property = await PropertyModel.findById(id);
+//   const formattedProperty = formatProperty(property);
+//   return response.status(200).send(formattedProperty);
+// });
+
+// module.exports = app;
