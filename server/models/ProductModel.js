867ebe2fe0f3533e7336c4678b01555dc442afc0
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-09-24 13:03:35 +1200

    fix: ProductSchema

--- server/models/ProductModel.js
+++ server/models/ProductModel.js
@@ -4,9 +4,9 @@ const ProductSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   img: { type: String, required: true },
-  isbn: { type: String, required: true },
-  price: { type: Number, required: true },
-  rating: { type: Number, required: true },
+  isbn: { type: Number, required: true },
+  price: { type: String, required: true },
+  rating: { type: String, required: true },
 });
 
 const ProductModel = mongoose.model("Product", ProductSchema);
