8029821945257c5515cf020f571be12f46283ae5
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-09-24 12:48:06 +1200

    complete: ProductModel

--- server/models/ProductModel.js
+++ server/models/ProductModel.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const ProductSchema = mongoose.Schema({
   title: { type: String, required: true },
-  img: { type: String, required: true },
-  price: { type: Number, required: true },
   description: { type: String, required: true },
-  // ==========
-  title: { type: String, required: true },
-  title: { type: String, required: true },
-  title: { type: String, required: true },
+  img: { type: String, required: true },
+  isbn: { type: String, required: true },
+  price: { type: Number, required: true },
+  rating: { type: Number, required: true },
 });
 
 const ProductModel = mongoose.model("Product", ProductSchema);
